88099ee822297f608d6eaf15b0a6d6a93e24fefc
Germán Fonseca <german.fonseca33@gmail.com>
2022-04-01 00:08:53 -0500

    Agregar archivos de proyecto.

---
+++ BancoMedellin/Server/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BancoMedellin.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        public DbSet<Usuario>? Usuarios { get; set; }
+    }
+}
---
+++ BancoMedellin/Shared/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BancoMedellin.Shared
+{
+    public class Usuario
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public ulong Dni { get; set; }
+        [Required, MaxLength(64)]
+        public string Nombre { get; set; } = string.Empty;
+        [Required, MaxLength(4)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
